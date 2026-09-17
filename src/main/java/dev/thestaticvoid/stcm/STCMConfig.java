09a0837fca9833c040f3a1b97527792c93d4e45d
fmbellomy <fmbellomy@quantumgarbage.com>
2026-09-17 13:50:12 -0500

    use actual enum config type so that the allowed values appear in config comments

--- src/main/java/dev/thestaticvoid/stcm/STCMConfig.java
+++ src/main/java/dev/thestaticvoid/stcm/STCMConfig.java
@@ -21,7 +21,7 @@ public final class STCMConfig {
     public final ModConfigSpec.ConfigValue<Integer> prospectorVerticalRange;
     public final ModConfigSpec.ConfigValue<Integer> prospectorMinDepositSize;
     public final ModConfigSpec.ConfigValue<Integer> prospectorCooldown;
-    public final ModConfigSpec.ConfigValue<ProspectorDistanceMode> prospectorDistanceMode;
+    public final ModConfigSpec.EnumValue<ProspectorDistanceMode> prospectorDistanceMode;
     public final ModConfigSpec.ConfigValue<Integer> sampleProximityRange;
     public final ModConfigSpec.ConfigValue<Boolean> preventRuinPlacement;
 
@@ -32,7 +32,7 @@ public final class STCMConfig {
         prospectorHorizontalRange = builder.comment("The horizontal radius for the prospector to scan").define("prospector_x_radius", 16);
         prospectorVerticalRange = builder.comment("The vertical radius for the prospector to scan").define("prospector_y_radius", 256);
         prospectorMinDepositSize = builder.comment("The minimum amount of connected blocks to be considered a deposit").define("prospector_min_deposit_size", 32);
-        prospectorDistanceMode = builder.comment("What method the prospector should use for calculating the distance of an ore deposit").define("prospector_distance_mode", ProspectorDistanceMode.XYZ);
+        prospectorDistanceMode = builder.comment("What method the prospector should use for calculating the distance of an ore deposit").defineEnum("prospector_distance_mode", ProspectorDistanceMode.XYZ, ProspectorDistanceMode.values());
         prospectorCooldown = builder.comment("The number of ticks to wait before you can use the prospector pick again.").define("prospector_cooldown", 100);
         sampleProximityRange = builder.comment("The distance between ore samples of the same type that duplicate waypoint creation should be blocked in").define("prospector_proximity", 16);
         preventRuinPlacement = builder.comment("Prevents Spectrum \"Bottle of Ruin\" from being placed in the world. Replaces the behavior of clicking on bedrock, mainly for servers").define("prevent_ruin", false);
